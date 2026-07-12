0a9764c4cac4b74914a6aa733c2ffe3d8a742e5b
Arata <49086220+arata-nvm@users.noreply.github.com>
2026-07-12 11:04:11 +0900

    chore: add check command

--- build.zig
+++ build.zig
@@ -18,17 +18,19 @@ pub fn build(b: *std.Build) void {
     });
     microps_mod.addImport("build_options", options.createModule());
 
+    const test_mod = b.createModule(.{
+        .root_source_file = b.path("src/test/test.zig"),
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+        .imports = &.{
+            .{ .name = "microps", .module = microps_mod },
+        },
+    });
+
     const test_exe = b.addExecutable(.{
         .name = "test",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/test/test.zig"),
-            .target = target,
-            .optimize = optimize,
-            .link_libc = true,
-            .imports = &.{
-                .{ .name = "microps", .module = microps_mod },
-            },
-        }),
+        .root_module = test_mod,
     });
     b.installArtifact(test_exe);
 
@@ -37,17 +39,27 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the test app");
     run_step.dependOn(&run_cmd.step);
 
+    const test_exe_check = b.addExecutable(.{
+        .name = "test",
+        .root_module = test_mod,
+    });
+
+    const check = b.step("check", "Check if test compiles");
+    check.dependOn(&test_exe_check.step);
+
+    const tap_mod = b.createModule(.{
+        .root_source_file = b.path("src/test/tap.zig"),
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+        .imports = &.{
+            .{ .name = "microps", .module = microps_mod },
+        },
+    });
+
     const tap_exe = b.addExecutable(.{
         .name = "tap",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/test/tap.zig"),
-            .target = target,
-            .optimize = optimize,
-            .link_libc = true,
-            .imports = &.{
-                .{ .name = "microps", .module = microps_mod },
-            },
-        }),
+        .root_module = tap_mod,
     });
     b.installArtifact(tap_exe);
 
@@ -66,14 +78,14 @@ pub fn build(b: *std.Build) void {
     const tap_cmd = b.addSystemCommand(&.{
         "sh", "-c",
         \\ [ "$(uname)" = Linux ] || { echo "tap is only supported on Linux"; exit 1; }
-        \\ip addr show tap0 2>/dev/null || (
-        \\  echo "Create 'tap0'"
-        \\  sudo ip tuntap add mode tap user $USER name tap0
-        \\  sudo sysctl -w net.ipv6.conf.tap0.disable_ipv6=1
-        \\  sudo ip addr add 192.0.2.1/24 dev tap0
-        \\  sudo ip link set tap0 up
-        \\  ip addr show tap0
-        \\)
+        \\ ip addr show tap0 2>/dev/null || (
+        \\   echo "Create 'tap0'"
+        \\   sudo ip tuntap add mode tap user $USER name tap0
+        \\   sudo sysctl -w net.ipv6.conf.tap0.disable_ipv6=1
+        \\   sudo ip addr add 192.0.2.1/24 dev tap0
+        \\   sudo ip link set tap0 up
+        \\   ip addr show tap0
+        \\ )
     });
     tap_cmd.has_side_effects = true;
     const tap_step = b.step("tap", "Create and configure tap0 device");
